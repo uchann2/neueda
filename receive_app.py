90527dcac7532e39f31622cf1449c0a52a1a0e45
Nj <nj.chandradasa@linnsystems.com>
2018-12-06 22:04:14 +0200

    received and create file

--- receive_app.py
+++ receive_app.py
@@ -3,20 +3,40 @@ import sys
 import threading
 
 host = ''        # Symbolic name meaning all available interfaces
-port = 12345     # Arbitrary non-privileged port
+port = 12346     # Arbitrary non-privileged port
 
 def handle_client(client_socket,client_addr):
     print('Connected by', client_addr)
+    msg = ''
+    filename = ''
+    length = ''
     while True:
         try:
-            data = client_socket.recv(1024)
-            if not data: break
-            print "Client Says: "+data
-            conn.sendall("Server Says:hi")
-
+            data = client_socket.recv(1)
+            if data == '|': 
+                break
+            filename =  filename + data
+        except socket.error:
+            print "Error Occured."
+    while True:
+        try:
+            data = client_socket.recv(1)
+            if data == '|': 
+                break
+            length =  length + data
+        except socket.error:
+            print "Error Occured."
+    output = open("./received/"+filename,"w+")
+    while True:
+        try:
+            data = client_socket.recv(int(length))
+            if not data:
+                break
+            output.write(data)
         except socket.error:
             print "Error Occured."
             break
+    output.close()
 
 if __name__ == "__main__":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
--- send_app.py
+++ send_app.py
@@ -1,10 +1,13 @@
 import socket
 
 host = socket.gethostname()
-port = 12345                   # The same port as used by the server
+port = 12346                   # The same port as used by the server
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((host, port))
-s.sendall(b'Hello, world')
-data = s.recv(1024)
+inputstring = 'Hello, world, hey yall'
+length = len(inputstring)
+sendstring = "File-1.txt|"+str(length)+"|"+ inputstring
+b = bytearray()
+b.extend(sendstring)
+s.sendall(b)
 s.close()
-print('Received', repr(data))
